3f19a787160d1057c23246b3e0cb01a75ef63420
uniho <uniho@users.noreply.github.com>
2018-09-26 09:17:34 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Unstated7
+
+This is the same thing as that, it goes without saying.
+
+## Overview
+
+Unstated7 is a small and lightweight library for 
+[React](https://facebook.github.io/react/).
+Unstated7 provides you great features as [Unstated](https://github.com/jamiebuilds/unstated), except Provider and Promise though.
+
+Unstated:
+> If you're like me, you're sick of all the ceremony around state management in React. Something that fits in well with the React way of thinking, but doesn't command some crazy architecture and methodology.
+> 
+> Component state is nice! It makes sense and people pick it up quickly:
+> ・・・
+> As a new React developer you might not know exactly how everything works, but you can get a general sense pretty quickly.
+> 
+> The only problem here is that we can't easily share this state with other components in our tree. Which is intentional! React components are designed to be very self-contained.
+> 
+> What would be great is if we could replicate the nice parts of React's component state API while sharing it across multiple components.
+> 
+> But how do we share values between components in React? Through "context".
+> ・・・
+> This is already pretty great. Once you get a little bit used to React's way of thinking, it makes total sense and it's very predictable.
+> 
+> But can we build on this pattern to make something even nicer?
+> ・・・
+> Well this is where Unstated comes in.
+
+
+## Installing
+
+Unstated7 fully supports in-browser, NodeJS (, Electron) , Browserify... usage. If you are using
+Unstated7 in your browser, simply include the script:
+
+### Browser
+
+```html
+<script src="unstated7.js"></script>
+```
+
+Or use CDN:
+
+```html
+<script src="https://unpkg.com/unstated7@latest/unstated7.js"</script>
+```
+
+### NodeJS, etc.
+
+Copy unstated7.js your PC, or run the following:
+
+```
+npm install unstated7
+```
+
+Simply use the Unstated7 module as you would any other module:
+
+```javascript
+var unstated7 = require("unstated7.js");
+```
+
+## Usage
+
+See attached sample HTML files. 
+This samples make sense to you if you are a React and Unstated user. 
+
+## Contribution  
+
+1. Fork it  
+2. Create your feature branch  
+3. Commit your changes  
+4. Push to the branch  
+5. Create new Pull Request
+
+## License
+
+MIT
---
+++ unstated7.js
@@ -0,0 +1,99 @@
+/*
+  unstated7.js
+
+  This is the same thing as that, it goes without saying. 
+  
+  by UNIHO.
+
+*/
+
+const unstated7 = (function() {
+  "use strict";
+  
+  function unstated7() {
+  }
+
+  unstated7._containers = {};
+  
+  // inject(Instance of Container, ...)
+  unstated7.inject = function() {
+    unstated7._containers = {};
+    Array.prototype.forEach.call(arguments, function(item) { 
+      unstated7._containers[item.constructor] = item;
+    }.bind(this));
+  }
+
+  // Container Class
+  unstated7.Container = function() {
+    this._listeners = [];
+  };
+  unstated7.Container.prototype.setState = function(updater, callback) {
+    const nextState = typeof updater === 'function' ?
+      updater(this.state) : updater;
+  
+    if (nextState == null) {
+      if (callback) callback();
+      return;
+    }
+  
+    //this.state = Object.assign({}, this.state, nextState);
+    for (let p in nextState) this.state[p] = nextState[p];
+  
+    let counter = this._listeners.length;
+    const finish = function() {
+      counter--;
+      if (callback && !counter) callback();
+    };
+    this._listeners.forEach(function(item) {
+      setTimeout(function() {
+        if (this._listeners.indexOf(item) < 0) {
+          finish();
+          return;
+        }  
+        item.forceUpdate(finish);
+      }.bind(this), 0);
+    }.bind(this));
+  };  
+  unstated7.Container.prototype.subscribe = function(item) {
+    if (this._listeners.indexOf(item) >= 0) return;
+    this._listeners.push(item);
+  };
+  unstated7.Container.prototype.unsubscribe = function(item) {
+    this._listeners = this._listeners.filter(function(f) { return f !== item });
+  };
+
+  // Subscribe Class
+  unstated7.Subscribe = function(props) {
+    React.Component.call(this, props);
+    this.instances = [];
+  };
+  Object.setPrototypeOf(unstated7.Subscribe.prototype, React.Component.prototype);
+  unstated7.Subscribe.prototype.componentWillUnmount = function() {
+    this.instances.forEach(function(item) {item.unsubscribe(this)});
+  };
+  unstated7.Subscribe.prototype.render = function() {
+    const to = this.props.to;
+    this.instances = to.map(function(item) {
+      let instance;
+      if (typeof item === 'object') {
+        instance = item;
+      } else if (item in unstated7._containers) {
+        instance = unstated7._containers[item];
+      } else {
+        instance = new item();
+        unstated7._containers[item] = instance;
+      }
+      instance.subscribe(this);
+      return instance;
+    }.bind(this));
+
+    //return this.props.children(...this.instances);
+    return this.props.children.apply(null, this.instances);
+  };
+
+  return unstated7;
+})();
+
+if (typeof module != "undefined" && module.exports != null) {
+  module.exports = unstated7;
+}
